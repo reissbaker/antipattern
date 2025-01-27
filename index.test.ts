adcb456388a057a24f9ff81babfb895810703ea9
Matt Baker <matthew.reiss.baker@gmail.com>
2025-01-27 15:42:29 -0800

    Add a mock/restore interface

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,6 @@
 import { it, describe, expect } from "vitest";
 import { memo } from "radash";
-import { registry, withMock } from "./index";
+import { registry, withMock, mock } from "./index";
 
 const r = registry(class {
   async hello() {
@@ -33,5 +33,15 @@ describe("withMock", () => {
       expect(await r.hello()).toBe("goodbye");
     });
     expect(await r.hello()).toBe("hello");
+  });
+});
+describe("mock", () => {
+  it("mocks the returned object and restores when restore is called", async () => {
+    const restore = mock(r, "hello", "goodbye");
+    expect(await r.hello()).toBe("goodbye");
+    expect(await r.foo()).toBe("goodbye world");
+    restore();
+    expect(await r.hello()).toBe("hello");
+    expect(await r.foo()).toBe("hello world");
   });
 });
--- index.ts
+++ index.ts
@@ -10,14 +10,26 @@ export async function withMock<T, K extends keyof T>(
   replacement: Awaited<ReturnType<T[K] extends (...args: any[]) => any ? T[K] : never>>,
   cb: () => any,
 ) {
+  const restore = mock(r, key, replacement);
+  try {
+    await Promise.resolve(cb());
+  } finally {
+    restore();
+  }
+}
+
+export function mock<T, K extends keyof T>(
+  r: IsAsync<T>,
+  key: K,
+  replacement: Awaited<ReturnType<T[K] extends (...args: any[]) => any ? T[K] : never>>,
+) {
   const original = r[key];
   // @ts-ignore
   r[key] = async function() {
     return replacement;
   }.bind(r);
-  try {
-    await Promise.resolve(cb());
-  } finally {
+
+  return () => {
     r[key] = original;
   }
 }
